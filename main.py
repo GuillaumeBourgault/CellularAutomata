ccd8e972aab320307201389f1e63760ad9c8589c
GuillaumeBourgault <skidoodlidoo@gmail.com>
2026-05-11 14:36:45 +0200

    grid is now moving up with updates

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib
 import itertools
 
 GRID_SIZE = (10, 10)
+RECTANGLE_SIZE = 1
 
 
 def build_grid(m: np.ndarray, grid_size=GRID_SIZE) -> matplotlib.figure.Figure:
@@ -13,24 +14,47 @@ def build_grid(m: np.ndarray, grid_size=GRID_SIZE) -> matplotlib.figure.Figure:
     ax.set_xlim(grid_size[0] + 1)
     ax.set_ylim(grid_size[1] + 1)
     ax.set_aspect("equal", "box")
-    rectangle_size = 1
-    for x, y in itertools.product(range(grid_size[0] + 1), range(grid_size[1] + 1)):
-        if x >= m.shape[1] or y >= m.shape[0]:
-            color = "white"
+    delta = m.shape[0] - GRID_SIZE[1] if m.shape[0] > GRID_SIZE[1] else 0
+    for y in range(grid_size[1]):
+        if y >= m.shape[0]:
+            ax = add_empty_row_to_fig(ax, y)
         else:
-            color = "black" if m[y, x] else "white"
-        rect = plt.Rectangle(
-            [x + 1, y + 1],
-            rectangle_size,
-            rectangle_size,
-            facecolor=color,
-            edgecolor="black",
-        )
-        ax.add_patch(rect)
+            ax = add_row_to_fig(ax, m[y + delta], y)
     ax.invert_xaxis()
     ax.xaxis.set_major_locator(plt.NullLocator())
     ax.yaxis.set_major_locator(plt.NullLocator())
     return fig
+
+
+def add_row_to_fig(
+    ax: matplotlib.figure.Figure, r: np.array, y: int
+) -> matplotlib.figure.Figure:
+    for x in range(GRID_SIZE[0]):
+        color = "black" if r[x] else "white"
+        rect = plt.Rectangle(
+            [x + 1, y + 1],
+            RECTANGLE_SIZE,
+            RECTANGLE_SIZE,
+            facecolor=color,
+            edgecolor="black",
+        )
+        ax.add_patch(rect)
+    return ax
+
+
+def add_empty_row_to_fig(
+    ax: matplotlib.figure.Figure, y: int
+) -> matplotlib.figure.Figure:
+    for x in range(GRID_SIZE[0] + 1):
+        rect = plt.Rectangle(
+            [x + 1, y + 1],
+            RECTANGLE_SIZE,
+            RECTANGLE_SIZE,
+            facecolor="white",
+            edgecolor="black",
+        )
+        ax.add_patch(rect)
+    return ax
 
 
 st.title("Live Updating Matplotlib Plot")
@@ -39,12 +63,13 @@ st.title("Live Updating Matplotlib Plot")
 plot_placeholder = st.empty()
 
 # Example evolving data
-x = np.linspace(0, 10, 100)
 
 # Infinite update loop
+m = np.random.rand(1, GRID_SIZE[1]) > 0.5
 for frame in range(1000):
-    m = np.random.rand(5, GRID_SIZE[1]) > 0.5
     fig = build_grid(m)
     plot_placeholder.pyplot(fig)
     plt.close(fig)
-    time.sleep(1)
+    time.sleep(0.2)
+    r = np.random.rand(1, GRID_SIZE[1]) > 0.5
+    m = np.vstack([m, r])
--- test.py
+++ test.py
@@ -2,3 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 import matplotlib
+
+GRID_SIZE = (10, 10)
+
+
+def add_new_row_to_matrix(m: np.ndarray, r: np.ndarray) -> np.ndarray:
+    m = np.vstack([m, r])
+    return m
+
+
+m = np.random.rand(5, GRID_SIZE[1]) > 0.5
+r = np.random.rand(1, GRID_SIZE[1]) > 0.5
+m = add_new_row_to_matrix(m, r)
