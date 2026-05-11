a265d70c64ec20edd3471e30651f310002f838f2
GuillaumeBourgault <skidoodlidoo@gmail.com>
2026-05-11 13:14:20 +0200

    integrated random plot in the main loop

--- main.py
+++ main.py
@@ -2,6 +2,36 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+import matplotlib
+import itertools
+
+GRID_SIZE = (10, 10)
+
+
+def build_grid(m: np.ndarray, grid_size=GRID_SIZE) -> matplotlib.figure.Figure:
+    fig, ax = plt.subplots(figsize=grid_size)
+    ax.set_xlim(grid_size[0] + 1)
+    ax.set_ylim(grid_size[1] + 1)
+    ax.set_aspect("equal", "box")
+    rectangle_size = 1
+    for x, y in itertools.product(range(grid_size[0] + 1), range(grid_size[1] + 1)):
+        if x >= m.shape[1] or y >= m.shape[0]:
+            color = "white"
+        else:
+            color = "black" if m[y, x] else "white"
+        rect = plt.Rectangle(
+            [x + 1, y + 1],
+            rectangle_size,
+            rectangle_size,
+            facecolor=color,
+            edgecolor="black",
+        )
+        ax.add_patch(rect)
+    ax.invert_xaxis()
+    ax.xaxis.set_major_locator(plt.NullLocator())
+    ax.yaxis.set_major_locator(plt.NullLocator())
+    return fig
+
 
 st.title("Live Updating Matplotlib Plot")
 
@@ -13,22 +43,8 @@ x = np.linspace(0, 10, 100)
 
 # Infinite update loop
 for frame in range(1000):
-
-    # Generate some changing data
-    y = np.sin(x + frame * 0.2)
-
-    # Create matplotlib figure
-    fig, ax = plt.subplots(figsize=(6, 4))
-    ax.plot(x, y)
-
-    ax.set_ylim(-1.2, 1.2)
-    ax.set_title(f"Frame {frame}")
-
-    # Display in Streamlit
+    m = np.random.rand(5, GRID_SIZE[1]) > 0.5
+    fig = build_grid(m)
     plot_placeholder.pyplot(fig)
-
-    # Important: close figure to avoid memory leak
     plt.close(fig)
-
-    # Wait before next update
     time.sleep(1)
--- test.py
+++ test.py
@@ -1,29 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
-
-grid_size = (10, 10)
-a = np.random.rand(5, grid_size[1]) > 0.5
-fig, ax = plt.subplots(figsize=grid_size)
-ax.set_xlim(grid_size[0] + 1)
-ax.set_ylim(grid_size[1] + 1)
-ax.set_aspect("equal", "box")
-rectangle_size = 1
-for x, y in itertools.product(range(grid_size[0] + 1), range(grid_size[1] + 1)):
-    if x >= a.shape[1] or y >= a.shape[0]:
-        color = "white"
-    else:
-        color = "black" if a[y, x] else "white"
-    rect = plt.Rectangle(
-        [x + 1, y + 1],
-        rectangle_size,
-        rectangle_size,
-        facecolor=color,
-        edgecolor="black",
-    )
-    ax.add_patch(rect)
-ax.invert_xaxis()
-ax.xaxis.set_major_locator(plt.NullLocator())
-ax.yaxis.set_major_locator(plt.NullLocator())
-print(a)
-plt.show()
+import matplotlib
